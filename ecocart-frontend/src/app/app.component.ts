afd49a60a9f02f36f4d07d246481fff295b999c1
Saran-Sandeep <115928655+Saran-Sandeep@users.noreply.github.com>
2025-02-19 07:14:24 +0530

    Added home section

--- ecocart-frontend/src/app/app.component.ts
+++ ecocart-frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './features/header/header.component';
-
+import { HeroComponent } from './features/hero/hero.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
